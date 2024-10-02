f409d28b2b7a1875c97e436e7389189bd04ebf48
DevikaShinith <devikashinith8@gmail.com>
2024-10-02 21:52:48 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,29 @@
+/*
+Program header
+objective: to evaluate quadratic eqn
+author: Devika Shinith
+date:2/10/2024
+*/
+#include <stdio.h>
+
+int countOnes(int num) {
+    int count = 0;
+    while (num) {
+        count += num & 1; 
+        num >>= 1;         
+    }
+    return count;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int result = countOnes(num);
+
+    printf("The number of 1's in the binary representation of %d is: %d\n", num, result);
+
+    return 0;
+}
